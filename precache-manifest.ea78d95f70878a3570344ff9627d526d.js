dd3c585336f3d83b01d9c624ddaf5e6f0c36bc70
SACHIKO YOKOYAMA <SACHIKO@MacBook-Pro-3.lan>
2020-09-09 18:33:51 -0700

    Updates

---
+++ precache-manifest.ea78d95f70878a3570344ff9627d526d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6133eaa0c0e79d03612462d6422b8c1c",
+    "url": "/react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "3f2979c5cd8673bfb393",
+    "url": "/react-tic-tac-toe/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d1a1622ba4da13b4c335",
+    "url": "/react-tic-tac-toe/static/js/2.69b12e40.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tic-tac-toe/static/js/2.69b12e40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f2979c5cd8673bfb393",
+    "url": "/react-tic-tac-toe/static/js/main.5c4207a1.chunk.js"
+  },
+  {
+    "revision": "929d476f7b1ee9149c3a",
+    "url": "/react-tic-tac-toe/static/js/runtime-main.db7cfeb9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-tic-tac-toe/static/media/logo.5d5d9eef.svg"
+  }
+]);
