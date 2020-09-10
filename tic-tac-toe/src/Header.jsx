5f76ae7ecefe13d46c8d8779e958a261a433ac75
SACHIKO YOKOYAMA <SACHIKO@MacBook-Pro-3.lan>
2020-09-09 18:59:56 -0700

    changed some files

---
+++ tic-tac-toe/src/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+function Header () {
+    return (
+        <header className="header">
+            <h1 className="title">Tic Tac Toe</h1>
+        </header>
+    )
+}
+
+export default Header;
